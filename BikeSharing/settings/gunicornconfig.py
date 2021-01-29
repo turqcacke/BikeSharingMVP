f1881c56e3abc9793f14b387641d6b2b71b4c2d2
turqcacke <60967566+turqcacke@users.noreply.github.com>
2021-01-29 15:29:41 +0500

    Upd: gunicorn config added

---
+++ BikeSharing/settings/gunicornconfig.py
@@ -0,0 +1,8 @@
+command = '/home/politouz/code/BikeSharingMVP/venv/bin/gunicorn'
+pythonpath = '/home/politouz/code/BikeSharingMVP/BikeSharing'
+bind = '127.0.0.1:8080'
+workers = 5
+user = 'politouz'
+limit_request_fields = 32000
+limit_request_field_size = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=BikeSharing.settings.prod'
