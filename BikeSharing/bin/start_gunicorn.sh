e053d1873a8cb8d55c5d6bcc90f10f2dad210e34
turqcacke <60967566+turqcacke@users.noreply.github.com>
2021-01-29 23:22:50 +0500

    Rebuild gunicorn.

--- BikeSharing/bin/start_gunicorn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /home/politouz/code/BikeSharingMVP/venv/bin/activate
-exec gunicorn -c "/home/politouz/code/BikeSharingMVP/BikeSharing/settings/gunicornconfig.py" --chdir /home/politouz/code/BikeSharingMVP BikeSharing.wsgi
--- BikeSharing/settings/gunicornconfig.py
+++
@@ -1,8 +0,0 @@
-command = '/home/politouz/code/BikeSharingMVP/venv/bin/gunicorn'
-pythonpath = '/home/politouz/code/BikeSharingMVP/BikeSharing'
-bind = '127.0.0.1:8081'
-workers = 5
-user = 'politouz'
-limit_request_fields = 32000
-limit_request_field_size = 0
-raw_env = 'DJANGO_SETTINGS_MODULE=BikeSharing.settings.prod'
