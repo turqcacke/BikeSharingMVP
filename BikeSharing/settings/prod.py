d6b1c481e744b41ce647836c89d704111b47a285
turqcacke <60967566+turqcacke@users.noreply.github.com>
2021-01-29 17:47:49 +0500

    Upd: switched on production settings

--- BikeSharing/settings/prod.py
+++ BikeSharing/settings/prod.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'BikeSharing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bs_db',
+        'USER': 'bs_user',
+        'PASSWORD': 'bs_user@postgres!',
     }
 }
 
